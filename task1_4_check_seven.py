d9a14d0294f60658d2c08cfd45d4dff36007ee71
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-20 09:29:38 +0500

    skfb

--- task1_4_check_seven.py
+++ task1_4_check_seven.py
@@ -1,3 +1,4 @@
+
 def check_seven(numbers: tuple) -> bool:
     """
     Check if the number 7 exists in a tuple.
@@ -6,4 +7,8 @@ def check_seven(numbers: tuple) -> bool:
     Returns:
         bool: True if 7 is in the tuple, False otherwise
     """
-    return
+    for i in numbers:
+        if i==7 :
+            return True
+    return False
+print(check_seven((1,7,2,42)))
