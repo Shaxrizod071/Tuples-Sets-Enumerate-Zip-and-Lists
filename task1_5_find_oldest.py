a00e902aaec90456a6c061cd096bf80e8eca31e9
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-21 23:34:01 +0500

    sdfk

--- task1_5_find_oldest.py
+++ task1_5_find_oldest.py
@@ -6,4 +6,6 @@ def find_oldest(people: list) -> tuple:
     Returns:
         tuple: A tuple containing (name, age) of the oldest person
     """
-    return
+    asd=max(people,key=lambda x: x[1])
+    return asd
+print(find_oldest([("Ali", 20), ("Sara", 12), ("John", 2)]))
