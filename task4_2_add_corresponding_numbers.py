8faebe8144b226e889cd9b4060eca4a675932e81
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-24 17:49:15 +0500

    skdfb

--- task4_2_add_corresponding_numbers.py
+++ task4_2_add_corresponding_numbers.py
@@ -7,4 +7,5 @@ def add_corresponding_numbers(list1: list, list2: list) -> list:
     Returns:
         list: List containing sum of corresponding numbers
     """
-    return
+    return [key+value for key,value in zip(list1, list2)]
+print(add_corresponding_numbers([1,2,3,4],[1,2,3,4]))
